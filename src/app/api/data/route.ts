2bd01dc34fa2b71f3375da26b37cfa4d4b18e213
nur-wahid123 <fajarnurwahid99@gmail.com>
2023-09-06 06:10:28 +0700

    plus potgrre

--- src/app/api/data/route.ts
+++ src/app/api/data/route.ts
@@ -1,11 +1,10 @@
+import { sql } from "@vercel/postgres";
 import { NextApiRequest } from "next";
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse as res } from "next/server";
 import pool from "./../database"
 
 export async function POST(request: Request, req: NextRequest) {
-    const conn = await pool.getConnection()
-    const [row, fields] = await conn.query("select * from barang");
-    conn.release()
-    return res.json({ message: "ini Data ya", row })
+    const {rows} = await sql`select * from kategori`;
+    return res.json({ message: "ini Data ya", rows })
 }
